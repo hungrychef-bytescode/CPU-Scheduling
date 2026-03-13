58aeceace9686cb6031aeb82a019ec311303e59d
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-03-13 21:36:28 +0800

    feat: added EventType enum and Event struct for the simulator engine

---
+++ schedsim/include/simulator.h
@@ -0,0 +1,18 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+typedef enum {
+    EVENT_ARRIVAL,
+    EVENT_COMPLETION,
+    EVENT_QUANTUM_EXPIRE,
+    EVENT_PRIORITY_BOOST
+} EventType;
+
+typedef struct Event {
+    int time;
+    EventType type;
+    Process *process;
+    struct Event *next
+} Event;
+
+#endif
